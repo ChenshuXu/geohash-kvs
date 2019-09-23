a2591559441480280d45bb07918374a0a8ee721f
Chenshu Xu <newton9001@gmail.com>
2019-09-23 11:01:26 +0800

    add NGeoHash library

---
+++ geohash/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NGeoHash;
+
+namespace geohash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Fort Worth
+            var location = new
+            {
+                latitude = 32.768799,
+                longitude = -97.309341,
+            };
+            var encoded = GeoHash.Encode(location.latitude, location.longitude); // "9vff3tms0"
+            Console.WriteLine("encoded=" + encoded);
+
+            var decoded = GeoHash.Decode("9vff3tms0");
+            var latitude = decoded.Coordinates.Lat;     // 32.768805027008057
+            Console.WriteLine("latitude=" + latitude);
+            var longitude = decoded.Coordinates.Lon;	// -97.309319972991943
+            Console.WriteLine("longitude=" + longitude);
+        }
+    }
+}
