38e636723746bf5a4fdbdec26f96099e59c6f0b6
Chenshu Xu <newton9001@gmail.com>
2019-09-23 21:40:54 +0800

    add geohash simulation database
    add crime data to simulate geohash database

---
+++ geohash/DataBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using NGeoHash;
+using Newtonsoft;
+using System.IO;
+using System.Linq;
+
+namespace geohash
+{
+    public class DataBase
+    {
+        // Key is encoded code
+        // Value is an array of coordinates
+        private IDictionary<string, List<Coordinates>> dict = new Dictionary<string, List<Coordinates>>();
+        private int numberOfCharsStart = 5;
+        private int maxCoordinatesInValue = 100;
+
+        public object JsonConvert { get; private set; }
+        public object Newtonsoft { get; private set; }
+
+
+        public void Add(double latitude, double longitude)
+        {
+            int numberOfChars = numberOfCharsStart;
+            // Get the key
+            string key = GeoHash.Encode(latitude, longitude, 5);
+            // Get the value
+            Coordinates coordinate = new Coordinates { Lat = latitude, Lon = longitude };
+
+            // Check how many coordinates under this key
+            // If more than maxCoordinatesInValue, encode one more level
+            while (dict.ContainsKey(key) && dict[key].Count >= maxCoordinatesInValue)
+            {
+                numberOfChars++;
+                key = GeoHash.Encode(latitude, longitude, numberOfChars);
+            }
+
+            // If less than maxCoordinatesInValue or not exist add to value
+            if (dict.ContainsKey(key))
+            {
+                dict[key].Add(coordinate);
+                Console.WriteLine(key);
+            }
+            else
+            {
+                List<Coordinates> value = new List<Coordinates> { coordinate };
+                dict.Add(key, value);
+                Console.WriteLine(key);
+            }
+        }
+
+
+        public void Display()
+        {
+            using (StreamWriter sw = new StreamWriter("dict.csv"))
+            {
+                foreach (var item in dict)
+                {
+                    //sw.WriteLine(item.Key + "," + item.Value.Count);
+
+                    foreach (var i in item.Value)
+                    {
+                        string str = item.Key + "," + item.Value.Count + "," + i.Lat.ToString() + "," + i.Lon.ToString();
+                        sw.WriteLine(str);
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
--- geohash/Program.cs
+++ geohash/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NGeoHash;
 
 namespace geohash
@@ -7,21 +8,82 @@ namespace geohash
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // Fort Worth
             var location = new
             {
                 latitude = 32.768799,
                 longitude = -97.309341,
             };
-            var encoded = GeoHash.Encode(location.latitude, location.longitude); // "9vff3tms0"
-            Console.WriteLine("encoded=" + encoded);
+            var encoded = GeoHash.Encode(location.latitude, location.longitude, 5);
+            Console.WriteLine("encoded = " + encoded);
 
-            var decoded = GeoHash.Decode("9vff3tms0");
-            var latitude = decoded.Coordinates.Lat;     // 32.768805027008057
-            Console.WriteLine("latitude=" + latitude);
-            var longitude = decoded.Coordinates.Lon;	// -97.309319972991943
-            Console.WriteLine("longitude=" + longitude);
+            var decoded = GeoHash.Decode("9v");
+            var latitude = decoded.Coordinates.Lat;
+            Console.WriteLine("latitude = " + latitude);
+            var longitude = decoded.Coordinates.Lon;
+            Console.WriteLine("longitude = " + longitude);
+
+            BoundingBox box = GeoHash.DecodeBbox(encoded);
+            var maxLat = box.Maximum.Lat;
+            var minLat = box.Minimum.Lat;
+            var maxLon = box.Maximum.Lon;
+            var minLon = box.Minimum.Lon;
+
+            Console.WriteLine("latitude range: " + minLat + ", " + maxLat);
+            Console.WriteLine("longitude range: " + minLon + ", " + maxLon);
+
+            // Measure the box size in meters
+            var oneSide = Measure(maxLat, minLon, minLat, minLon);
+            var anotherSide = Measure(maxLat, maxLon, maxLat, minLon);
+            Console.WriteLine("box size: " + oneSide + " * " + anotherSide);
+
+            CrimeDataTest();
+        }
+
+        public static double Measure(double lat1, double lon1, double lat2, double lon2)
+        {  // generally used geo measurement function
+            var R = 6378.137; // Radius of earth in KM
+            var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+            var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+            Math.Cos(lat1 * Math.PI / 180) * Math.Cos(lat2 * Math.PI / 180) *
+            Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c;
+            return d * 1000.0; // meters
+        }
+
+        public static void CrimeDataTest()
+        {
+            string path = "/Users/chenshuxu/Projects/geohash-kvs/geohash/Crimes_-_2019.csv";
+            DataBase database = new DataBase();
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            //Console.WriteLine(lines[1]);
+            for (int i=1; i<lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] columns = line.Split(',');
+                string LatStr = columns[19];
+                string LonStr = columns[20];
+                if (LatStr != "" && LonStr != "")
+                {
+                    double lat;
+                    double lon;
+                    //Console.WriteLine(LatStr + LonStr);
+                    // Filter out errors
+                    try
+                    {
+                        lat = Convert.ToDouble(LatStr);
+                        lon = Convert.ToDouble(LonStr);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    database.Add(lat, lon);
+                }
+            }
+            database.Display();
         }
     }
 }
