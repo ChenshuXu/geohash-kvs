d4b9bebeeb5f799572e167f3e2f554cf819d4577
Chenshu Xu <newton9001@gmail.com>
2019-09-25 18:51:32 +0800

    Bounding circle function

--- geohash/DataBase.cs
+++ geohash/DataBase.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using NGeoHash;
-using Newtonsoft;
 using System.IO;
-using System.Linq;
 
 namespace geohash
 {
@@ -17,7 +15,7 @@ namespace geohash
         private int maxCoordinatesInValue = 1000;
         private int maxNumberOfChar = 9;
 
-        public void Add(double latitude, double longitude, int numberOfChars = 1)
+        public void Add( double latitude, double longitude, int numberOfChars = 1 )
         {
             // Get the key
             string key = GeoHash.Encode(latitude, longitude, numberOfChars);
@@ -28,21 +26,21 @@ namespace geohash
             {
                 List<Coordinates> value = new List<Coordinates> { coordinate };
                 Dict.Add(key, value);
-                Console.WriteLine("Add new key " + key);
+                //Console.WriteLine("Add new key " + key);
                 return;
             }
 
             if (Dict.ContainsKey(key) && Dict[key].Count < maxCoordinatesInValue)
             {
                 Dict[key].Add(coordinate);
-                Console.WriteLine("Add existent key " + key + ", " + Dict[key].Count);
+                //Console.WriteLine("Add existent key " + key + ", " + Dict[key].Count);
                 return;
             }
 
             if (numberOfChars >= maxNumberOfChar)
             {
                 Dict[key].Add(coordinate);
-                Console.WriteLine("Add existent key " + key + ", " + Dict[key].Count);
+                //Console.WriteLine("Add existent key " + key + ", " + Dict[key].Count);
                 return;
             }
 
@@ -69,6 +67,11 @@ namespace geohash
             }
         }
 
+        /**
+         * Store dictionary in a file
+         *
+         *
+         */
         public void Display()
         {
             using (StreamWriter sw = new StreamWriter("dict.csv"))
@@ -84,6 +87,157 @@ namespace geohash
                     }
                 }
             }
+        }
+
+        /**
+        * Bounding Circle
+        *
+        * Return all the hashString covered by the circle in numberOfChars
+        * @param {double} lat
+        * @param {double} lon
+        * @param {double} radius in meters
+        * @param {int} numberOfChars
+        * @returns {string[]}
+        */
+
+        public static string[] Bcircle( double latitude, double longitude, double radius, int numberOfChars = 9 )
+        {
+            var hashList = new List<string>();
+            string hashCenter = GeoHash.Encode(latitude, longitude, numberOfChars);
+            hashList.Add(hashCenter);
+
+            GeohashDecodeResult latLon = GeoHash.Decode(hashCenter);
+
+            // Find left and right end
+            // Find west(left) end
+            Coordinates leftCoor = MetersToCoordinate(latitude, longitude, radius, 270);
+            string hashLeft = GeoHash.Encode(leftCoor.Lat, leftCoor.Lon, numberOfChars);
+            BoundingBox boxLeft = GeoHash.DecodeBbox(hashLeft);
+
+            // Find east(right) end
+            Coordinates rightCoor = MetersToCoordinate(latitude, longitude, radius, 90);
+            string hashRight = GeoHash.Encode(rightCoor.Lat, rightCoor.Lon, numberOfChars);
+            BoundingBox boxRight = GeoHash.DecodeBbox(hashRight);
+
+            // Find steps(left to right)
+            double perLon = latLon.Error.Lon * 2; // box size in west-east direction
+            var lonStep = Math.Round((boxRight.Minimum.Lon - boxLeft.Minimum.Lon) / perLon);
+
+            double perLat = latLon.Error.Lat * 2; // box size in north–south direction
+
+            
+
+            for ( var lon = 0; lon <= lonStep; lon++ )
+            {
+                // Find current box
+                string currentBoxHash = GeoHash.Neighbor(hashLeft, new[] { 0, lon });
+                BoundingBox currentBox = GeoHash.DecodeBbox(currentBoxHash);
+
+                // Find north(upper) end
+                // Find up neighbor
+                // Check if in range
+                int i = 0;
+                BoundingBox upBox = currentBox;
+                string upBoxHash = currentBoxHash;
+                while (BoxInRange(upBox, latitude, longitude, radius))
+                {
+                    hashList.Add(upBoxHash);
+                    //Console.WriteLine("Add+ " + upBoxHash);
+                    i++;
+                    upBoxHash = GeoHash.Neighbor(currentBoxHash, new[] { i, 0 });
+                    upBox = GeoHash.DecodeBbox(upBoxHash);
+                }
+
+                // Find south(down) end
+                // Find steps(north to south)
+                int j = 0;
+                BoundingBox downBox = currentBox;
+                string downBoxHash = currentBoxHash;
+                while (BoxInRange(downBox, latitude, longitude, radius))
+                {
+                    hashList.Add(downBoxHash);
+                    //Console.WriteLine("Add- " + downBoxHash);
+                    j--;
+                    downBoxHash = GeoHash.Neighbor(currentBoxHash, new[] { j, 0 });
+                    downBox = GeoHash.DecodeBbox(downBoxHash);
+                }
+                //Console.WriteLine("one loop");
+            }
+
+
+
+            return hashList.ToArray();
+        }
+
+        /**
+         * Convert meters to coordinate in direction
+         *
+         * Return coordinates
+         * @param {double} lat
+         * @param {double} lon
+         * @param {double} d (distance in meter)
+         * @param {double} bearing (clockwise from north in degree)
+         */
+        public static Coordinates MetersToCoordinate( double lat, double lon, double d, double brng )
+        {
+            double R = 6378137.0;
+
+            // convert to radian
+            double lat2 = lat * Math.PI / 180;
+            double lon2 = lon * Math.PI / 180;
+            double brng2 = brng * Math.PI / 180;
+
+            // Calculate and convert to degree
+            var lat3 = Math.Asin(Math.Sin(lat2) * Math.Cos(d / R) +
+                    Math.Cos(lat2) * Math.Sin(d / R) * Math.Cos(brng2)) * 180.0/Math.PI;
+            var lon3 = (lon2 + Math.Atan2(Math.Sin(brng2) * Math.Sin(d / R) * Math.Cos(lat2),
+                        Math.Cos(d / R) - Math.Sin(lat2) * Math.Sin(lat2))) * 180/Math.PI;
+
+            // Normalise to −180 +180
+            double lat4 = (lat3 + 540.0) % 360 - 180.0;
+            double lon4 = (lon3 + 540.0) % 360 - 180.0;
+
+            return new NGeoHash.Coordinates { Lat = lat4, Lon = lon4 };
+        }
+
+        /**
+         * Measure the distance between two points
+         *
+         * Return distance in meters
+         */
+        public static double Measure( double lat1, double lon1, double lat2, double lon2 )
+        {
+            // generally used geo measurement function
+            var R = 6378.137; // Radius of earth in KM
+            var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+            var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+            Math.Cos(lat1 * Math.PI / 180) * Math.Cos(lat2 * Math.PI / 180) *
+            Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c;
+            return d * 1000.0; // meters
+        }
+
+        /**
+         * ***Need improvement***
+         * Check whether the box overlaps with circle
+         *
+         *
+         */
+        public static Boolean BoxInRange( BoundingBox box, double lat1, double lon1, double distance )
+        {
+            // distance between centers
+            double middleLat = ( box.Maximum.Lat + box.Minimum.Lat ) / 2;
+            double middleLon = ( box.Maximum.Lon + box.Minimum.Lat ) / 2;
+            double dR = Measure( middleLat, middleLon, lat1, lon1 );
+
+            // 4 rectangle vertex in circle
+            double d1 = Measure( box.Maximum.Lat, box.Maximum.Lon, lat1, lon1 );
+            double d2 = Measure( box.Maximum.Lat, box.Minimum.Lon, lat1, lon1 );
+            double d3 = Measure( box.Minimum.Lat, box.Maximum.Lon, lat1, lon1 );
+            double d4 = Measure( box.Minimum.Lat, box.Minimum.Lon, lat1, lon1 );
+            return ( d1 <= distance || d2 <= distance || d3 <= distance || d4 <= distance );
         }
     }
 }
--- geohash/Program.cs
+++ geohash/Program.cs
@@ -10,13 +10,35 @@ namespace geohash
         {
             var location = new
             {
-                latitude = 32.768799,
-                longitude = -97.309341,
+                latitude = 41.732733557,
+                longitude = -87.551320357,
             };
-            var encoded = GeoHash.Encode(location.latitude, location.longitude, 5);
+            int level = 7;
+            double radius = 2000;
+            
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            string[] hashList = DataBase.Bcircle(location.latitude, location.longitude, radius, level);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            foreach (var h in hashList)
+            {
+                Console.WriteLine(h);
+                //BoundingBox box2 = GeoHash.DecodeBbox(h);
+                //if (DataBase.BoxInRange(box2, location.latitude, location.longitude, radius))
+                //{
+                //    Console.WriteLine("yes");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("no");
+                //}
+            }
+
+            var encoded = GeoHash.Encode(location.latitude, location.longitude, level);
             Console.WriteLine("encoded = " + encoded);
 
-            var decoded = GeoHash.Decode("9v");
+            var decoded = GeoHash.Decode(encoded);
             var latitude = decoded.Coordinates.Lat;
             Console.WriteLine("latitude = " + latitude);
             var longitude = decoded.Coordinates.Lon;
@@ -28,38 +50,68 @@ namespace geohash
             var maxLon = box.Maximum.Lon;
             var minLon = box.Minimum.Lon;
 
-            Console.WriteLine("latitude range: " + minLat + ", " + maxLat);
-            Console.WriteLine("longitude range: " + minLon + ", " + maxLon);
-
             // Measure the box size in meters
-            var oneSide = Measure(maxLat, minLon, minLat, minLon);
-            var anotherSide = Measure(maxLat, maxLon, maxLat, minLon);
+            var oneSide = DataBase.Measure(maxLat, minLon, minLat, minLon);
+            var anotherSide = DataBase.Measure(maxLat, maxLon, maxLat, minLon);
             Console.WriteLine("box size: " + oneSide + " * " + anotherSide);
-
-            CrimeDataTest();
-        }
-
-        public static double Measure(double lat1, double lon1, double lat2, double lon2)
-        {  // generally used geo measurement function
-            var R = 6378.137; // Radius of earth in KM
-            var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-            var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
-            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
-            Math.Cos(lat1 * Math.PI / 180) * Math.Cos(lat2 * Math.PI / 180) *
-            Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-            var d = R * c;
-            return d * 1000.0; // meters
+            Console.WriteLine("radius " + radius + "m level " + level);
+            Console.WriteLine("Time elapsed: " + elapsedMs+"ms | " + hashList.Length + " results");
         }
 
         public static void CrimeDataTest()
         {
-            string path = "../../../Resources/Crimes_-_2019.csv";
             DataBase database = new DataBase();
+            //AddDatasetSmall(database);
+            //AddDataset1(database);
+            //AddDataset2(database);
+            //database.Display();
+        }
 
+        static void AddDatasetSmall(DataBase database)
+        {
+            string path = "../../../Resources/Crimes_-_2019.csv";
             string[] lines = System.IO.File.ReadAllLines(path);
             //Console.WriteLine(lines[1]);
-            for (int i=1; i<lines.Length; i++)
+            int count = 0;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] columns = line.Split(',');
+                string LatStr = columns[19];
+                string LonStr = columns[20];
+                if (LatStr != "" && LonStr != "")
+                {
+                    double lat;
+                    double lon;
+                    //Console.WriteLine(LatStr + LonStr);
+                    // Filter out errors
+                    try
+                    {
+                        lat = Convert.ToDouble(LatStr);
+                        lon = Convert.ToDouble(LonStr);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    database.Add(lat, lon);
+                    count++;
+                }
+
+                if(count > 5000)
+                {
+                    return;
+                }
+            }
+            
+        }
+
+        static void AddDataset1(DataBase database)
+        {
+            string path = "../../../Resources/Crimes_-_2019.csv";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            //Console.WriteLine(lines[1]);
+            for (int i = 1; i < lines.Length; i++)
             {
                 string line = lines[i];
                 string[] columns = line.Split(',');
@@ -83,12 +135,14 @@ namespace geohash
                     database.Add(lat, lon);
                 }
             }
-            
+        }
 
-            path = "../../../Resources/crime.csv";
+        static void AddDataset2(DataBase database)
+        {
+            string path = "../../../Resources/crime.csv";
             string[] lines2 = System.IO.File.ReadAllLines(path);
             //Console.WriteLine(lines2[1]);
-            
+
             for (int i = 1; i < lines2.Length; i++)
             {
                 string line = lines2[i];
@@ -113,8 +167,6 @@ namespace geohash
                     database.Add(lat, lon);
                 }
             }
-            database.Display();
         }
-
     }
 }
