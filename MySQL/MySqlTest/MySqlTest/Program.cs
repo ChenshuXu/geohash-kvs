4a6f29ec8207df02da3f802ffdb951d3217a3a59
Newton <newton9001@gmail.com>
2019-10-30 16:14:31 +0800

    setup simple mysql connection test project

---
+++ MySQL/MySqlTest/MySqlTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySqlTest
+{
+    class Program
+    {
+        const string root_password = "bG43JPmBrY92";
+        const string user1_name = "xcs";
+        const string user1_password = "K7AA8XQS9wZq";
+
+        static void Main(string[] args)
+        {
+            string connStr = "server=127.0.0.1;uid=root;" +
+                "pwd=" + root_password + ";database=world";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
