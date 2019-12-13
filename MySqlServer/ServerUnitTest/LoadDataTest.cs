d014eae8b2e86494245fffcdc79a37236de6ecf7
Chenshu Xu <newton9001@gmail.com>
2019-12-13 14:22:47 +0800

    put load data test into one file

--- MySqlServer/ServerUnitTest/LoadDataTest.cs
+++ MySqlServer/ServerUnitTest/LoadDataTest.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MySqlServer;
+using TSQL;
+using TSQL.Tokens;
 
 namespace ServerUnitTest
 {
     [TestClass]
-    public class ClientSessionTest
+    public class LoadDataTest
     {
+        [TestMethod]
+        public void TestParserBasic()
+        {
+            string filePath = "../../../Resources/imptest-5000.txt";
+            string query = "LOAD DATA LOCAL INFILE '" + filePath + "' INTO TABLE imptest FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'";
+            List<TSQLToken> tokens = TSQLTokenizer.ParseTokens(query);
+            LoadDataParser parser = new LoadDataParser(tokens);
+
+            Assert.AreEqual(filePath, parser.file_name);
+            Assert.AreEqual("imptest", parser.table_name);
+            Assert.IsFalse(parser.fields_optionally_enclosed_by);
+            Assert.AreEqual(",", parser.fields_terminated_by);
+            Assert.AreEqual("\n", parser.lines_terminated_by);
+        }
+
+        [TestMethod]
+        public void TestParserComplex()
+        {
+            string query = "LOAD DATA LOCAL INFILE 'abc.txt' INTO TABLE imptest FIELDS TERMINATED BY 'aaa' OPTIONALLY ENCLOSED BY 'b' ESCAPED BY 'c' LINES STARTING BY 'eee' TERMINATED BY '\n'";
+            List<TSQLToken> tokens = TSQLTokenizer.ParseTokens(query);
+            LoadDataParser parser = new LoadDataParser(tokens);
+
+            Assert.AreEqual("abc.txt", parser.file_name);
+            Assert.AreEqual("imptest", parser.table_name);
+            Assert.AreEqual("aaa", parser.fields_terminated_by);
+            Assert.AreEqual("b", parser.fields_enclosed_by);
+            Assert.IsTrue(parser.fields_optionally_enclosed_by);
+            Assert.AreEqual("c", parser.fields_escaped_by);
+            Assert.AreEqual("eee", parser.lines_starting_by);
+            Assert.AreEqual("\n", parser.lines_terminated_by);
+        }
+
+        [TestMethod]
+        public void TestParserErrorHanding()
+        {
+            // TODO: No subclause after fields
+            string query = "LOAD DATA LOCAL INFILE 'abc.txt' INTO TABLE imptest FIELDS LINES TERMINATED BY '\n'";
+
+        }
+
         [TestMethod]
         public void TestParseLinesBasic()
         {
@@ -28,12 +71,13 @@ namespace ServerUnitTest
                 "15,VQTMAXWCDK"
             };
 
-            LoadDataParser parser = new LoadDataParser();
-            parser.lines_starting_by = "";
-            parser.lines_terminated_by = "\n";
+            string query = "LOAD DATA LOCAL INFILE 'myfile.txt' INTO TABLE imptest FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'";
+            List<TSQLToken> tokens = TSQLTokenizer.ParseTokens(query);
+            LoadDataParser parser = new LoadDataParser(tokens);
+            Assert.AreEqual("\n", parser.lines_terminated_by);
 
             string[] actual = ClientSession.ParseLines(file_string, parser);
-            foreach(var a in actual)
+            foreach (var a in actual)
             {
                 Console.WriteLine(a);
             }
@@ -59,9 +103,11 @@ zzz!15,VQTMAXWCDKll";
                 "15,VQTMAXWCDK"
             };
 
-            LoadDataParser parser = new LoadDataParser();
-            parser.lines_starting_by = "zzz!";
-            parser.lines_terminated_by = "ll";
+            string query = "LOAD DATA LOCAL INFILE 'myfile.txt' INTO TABLE imptest FIELDS TERMINATED BY ',' LINES STARTING BY 'zzz!' TERMINATED BY 'll'";
+            List<TSQLToken> tokens = TSQLTokenizer.ParseTokens(query);
+            LoadDataParser parser = new LoadDataParser(tokens);
+            Assert.AreEqual("zzz!", parser.lines_starting_by);
+            Assert.AreEqual("ll", parser.lines_terminated_by);
 
             string[] actual = ClientSession.ParseLines(file_string, parser);
             foreach (var a in actual)
@@ -79,7 +125,7 @@ zzz!15,VQTMAXWCDKll";
             parser.fields_terminated_by = ",";
             parser.fields_enclosed_by = "\"";
             parser.fields_escaped_by = "\\";
-            
+
             string line;
             string[] expected;
             string[] actual;
@@ -101,7 +147,7 @@ zzz!15,VQTMAXWCDKll";
             actual = ClientSession.ParseFields(line, parser);
             Assert.IsTrue(actual.SequenceEqual(expected));
 
-            
+
             // "2","a string containing a , comma","102.20"
             line = "\"2\",\"a string containing a , comma\",\"102.20\"";
             Console.WriteLine(line);
@@ -113,7 +159,7 @@ zzz!15,VQTMAXWCDKll";
             };
             actual = ClientSession.ParseFields(line, parser);
             Assert.IsTrue(actual.SequenceEqual(expected));
-            
+
             // "3","a string containing a \" quote","102.20"
             line = "\"3\",\"a string containing a \\\" quote\",\"102.20\"";
             Console.WriteLine(line);
@@ -125,7 +171,7 @@ zzz!15,VQTMAXWCDKll";
             };
             actual = ClientSession.ParseFields(line, parser);
             Assert.IsTrue(actual.SequenceEqual(expected));
-            
+
             // "4","a string containing a \", quote and comma","102.20"
             line = "\"4\",\"a string containing a \\\", quote and comma\",\"102.20\"";
             Console.WriteLine(line);
@@ -138,7 +184,7 @@ zzz!15,VQTMAXWCDKll";
             actual = ClientSession.ParseFields(line, parser);
             Assert.IsTrue(actual.SequenceEqual(expected));
             /*
-             * not support yet
+             * TODO: not support optionally yet
             // If you specify OPTIONALLY, the ENCLOSED BY character is used
             // only to enclose values from columns that have a string data type
             parser.fields_optionally_enclosed_by = true;
